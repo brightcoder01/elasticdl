5e995742b51c7df4f18a76794b395d7404ad4f4d
brightcoder01 <bright.gong@outlook.com>
2020-12-24 20:54:34 +0800

    Update gen_dataset.sh

--- scripts/gen_dataset.sh
+++ scripts/gen_dataset.sh
@@ -15,13 +15,17 @@
 # Generate mnist dataset
 DATA_PATH=$1
 
-python /scripts/image_label.py --dataset mnist \
+python ./elasticdl/python/data/recordio_gen/image_label.py --dataset mnist \
         --records_per_shard 4096 "$DATA_PATH"
 
 # Generate frappe dataset
-python /scripts/frappe_recordio_gen.py --data /root/.keras/datasets \
+python ./elasticdl/python/data/recordio_gen/frappe_recordio_gen.py --data /root/.keras/datasets \
     --output_dir "$DATA_PATH"/frappe
 
 # Generate heart dataset
-python /scripts/heart_recordio_gen.py --data_dir /root/.keras/datasets \
+python ./elasticdl/python/data/recordio_gen/heart_recordio_gen.py --data_dir /root/.keras/datasets \
     --output_dir "$DATA_PATH"/heart
+
+# Generate census dataset
+python ./elasticdl/python/data/recordio_gen/census_recordio_gen.py --data_dir /root/.keras/datasets \
+    --output_dir "$DATA_PATH"/census
